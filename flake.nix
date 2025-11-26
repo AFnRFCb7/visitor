dd300d05eb7077164c11cda3271c307f4fd5f12f
Emory Merryman <emory.merryman@gmail.com>
2025-11-26 18:02:02 -0500

    wtf

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
                                                 let
                                                     type = builtins.typeOf value ;
                                                     in
-                                                        if builtins.hasAttr ( if builtins.trace type type == "string" then value else "NOT" ) visitors then
+                                                        if builtins.hasAttr type visitors then
                                                             if type == "list" then builtins.genList ( index : visit ( builtins.concatLists [ path [ index ] ] ) ( builtins.elemAt value index ) ) ( builtins.length value )
                                                             else if type == "set" then builtins.mapAttrs ( name : value : visit ( builtins.concatLists [ path [ name ] ] ) value ) value
                                                             else builtins.getAttr type visitors path value
